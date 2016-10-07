84881f1316e51b1b3167b2042b85c42069ae1439
tbu <tobias.buchinger@iw.htwchur.ch>
2016-10-07 13:45:14 +0200

    add notex dybe ex 2016.10.07

--- 10_d3Einfuehrung/3_BarChart.js
+++ 10_d3Einfuehrung/3_BarChart.js
@@ -1,29 +1,42 @@
-//the data set
+//HA: the data set
 var data = [1, 2, 3, 4, 5, 6];
 
-//d3 basic select
+//HA: d3 basic select
 var mychart=d3.select(".chart");
-//create empty array of selection
+
+// leeres Array mit unauffindbarem Tag erzeugen.
+//HA: create empty array of selection
 var mydata=mychart.selectAll("somethingunfindable");
-//connect to data
+
+// Daten in den bisher leeren Array selectAll einfüllen.
+//HA: connect to data
 var mylinked=mydata.data(data);
-//fill in data content (in an attribute called '_data_')
+
+// Leere Elemente
+//HA: fill in data content (in an attribute called '_data_')
 var mycontent=mylinked.enter();
-//append a div element for each data set
+
+// Für jedes Element des Arrays ein leeres DIV kreieren und im OBJ mynewdivs speichern.
+//HA: append a div element for each data set
 var mynewdivs=mycontent.append("div");
-//set the style element 'width' of all divs individually by calling a function instead of giving a fixed value
+
+// Für jedes der neuen DIVs einen eigenen Style mit dem Schlüssel 'width' kreieren.
+//HA: set the style element 'width' of all divs individually by calling a function instead of giving a fixed value
 var mystyle=mynewdivs.style("width", datasetwidth);
-//set the data set as a text of the divs individually
+//HA: set the data set as a text of the divs individually
 mystyle.text(datasetText);
 
-//this function calculates the width of a single div
-//it is called by the style operation
+
+// Wenn 'datasetwidth' erreicht wird, wird die FKT aufgerufen. Ihr werden mit dataset die Daten übergeben.
+// Die FKT giebt eine String aus, der den Nennwert des Datenelements * 10 px ["30px"] ausgibt.
+//HA: this function calculates the width of a single div
+//HA: it is called by the style operation
 function datasetwidth (dataset) {
 	return dataset * 10 + "px";
 }
 
-//this function calculates the text of a single div
-//it is called by the text operation
+//HA: this function calculates the text of a single div
+//HA: it is called by the text operation
 function datasetText(dataset) {
 	return dataset;
 }
